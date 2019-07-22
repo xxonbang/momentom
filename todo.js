453c8f85b3440ed320dac80cbdddcbf4fa9c0b05
xxonbang <mackulri@gmail.com>
2019-07-22 22:38:09 +0900

    3.6 강의 끝까지 코드 작성완료

--- todo.js
+++ todo.js
@@ -1,8 +1,14 @@
 const toDoForm = document.querySelector(".js-toDoForm");
 const toDoInput = toDoForm.querySelector("input");
-const toDoList = document.querySelector("#ul");
+const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
+
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
 
 function handleSubmit(event) {
     event.preventDefault();
@@ -12,21 +18,31 @@ function handleSubmit(event) {
 }
 
 function paintToDo(text) {
-    console.log(text);
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerHTML = `x`;
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerText = `DELETE`;
     span.innerText = text;
-    li.appendChild(span);
     li.appendChild(delBtn);
+    li.appendChild(span);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if (toDos !== null) {
-
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(toDo => {
+            paintToDo(toDo.text);
+        });
     }
 }
 
