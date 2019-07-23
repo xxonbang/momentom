ed9db1231fed69898616c07bc07ad2fdbce0d142
Byeongcheol Son <mackulri@gmail.com>
2019-07-23 17:40:23 +0900

    finished

--- todo.js
+++ todo.js
@@ -4,7 +4,19 @@ const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
 
-const toDos = [];
+let toDos = [];
+
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function (toDo) {
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
 
 function saveToDos() {
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -23,6 +35,7 @@ function paintToDo(text) {
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     delBtn.innerText = `DELETE`;
+    delBtn.addEventListener("click", deleteToDo)
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
